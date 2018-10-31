c5fa5b7f431cf1d2ec689653bb6ae752887b5f48
dragonfiga <dragonfiga@126.com>
2018-10-31 12:49:40 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,11 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
+define('APIKEY', 'AIzaSyBy2VkQXTgCWkfWfgejVUcKH5GuO2DFAds');
 define('GJ_CODE', 'JP');
 define('SITE_NAME', '大爷常来玩儿啊');
 define('TITLENAME', 'DouDouTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
+
+
